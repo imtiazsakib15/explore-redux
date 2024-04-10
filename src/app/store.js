b1ad007b76f234e43790d1d284c5bb42e94679a7
imtiazsakib15 <sakib.imtiaz15@gmail.com>
2024-04-10 22:36:29 +0600

    Redux logger middleware added

--- src/app/store.js
+++ src/app/store.js
@@ -1,12 +1,15 @@
 import { configureStore } from "@reduxjs/toolkit";
 import counterReducer from "../features/counter/counterSlice";
 import postReducer from "../features/posts/postSlice";
+import reduxLogger from "redux-logger";
 
 const store = configureStore({
   reducer: {
     counter: counterReducer,
     posts: postReducer,
   },
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware().concat(reduxLogger),
 });
 
 export default store;
